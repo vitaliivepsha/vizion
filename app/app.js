79279b68a9a80301c716812b4118e963af4f7536
Artem <nicktlibra@gmail.com>
2023-04-10 10:26:33 +0300

    faq (desktop)

--- app/app.js
+++ app/app.js
@@ -24,14 +24,13 @@ if (process.env.NODE_ENV !== 'production') {
     require('./assets/templates/layouts/video-gallery.html');
     require('./assets/templates/layouts/video-gallery-article.html');
     require('./assets/templates/layouts/contacts.html');
+    require('./assets/templates/layouts/faq.html');
 
     // old
     require('./assets/templates/layouts/delivery-payment.html');
     require('./assets/templates/layouts/warranty.html');
     require('./assets/templates/layouts/purchase.html');
-    require('./assets/templates/layouts/faq.html');
     require('./assets/templates/layouts/checkout.html');
-
     require('./assets/templates/layouts/catalog-list.html');
     require('./assets/templates/layouts/price.html');
 }
